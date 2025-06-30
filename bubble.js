ce56d6f4bc83b825192a4f740099c919bf87dc96
Alexaverse <anushkaa22kumari@gmail.com>
2025-06-30 13:03:49 +0530

    completed

---
+++ bubble.js
@@ -0,0 +1,44 @@
+function makeBubble(){
+    var clutter="";
+for(var i = 1;i<=234;i++){
+    clutter+=`<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+}
+document.querySelector("#pbot").innerHTML=clutter;
+}
+function runTimer(){
+    var timer=4;
+    var time = setInterval(function(){
+        if(timer>0){
+            timer--;
+        }
+        else{
+            clearInterval(time);
+            document.querySelector("#pbot").innerHTML=`<h1>GAME OVER</h1>`
+        }
+        document.querySelector("#value").textContent=timer;    
+    },1000)
+}
+var hitrn=0;
+function Hit(){
+    hitrn = Math.floor(Math.random()*10)
+    document.querySelector("#hitval").textContent=hitrn;
+
+}
+var score=0;
+function increaseScore(){   
+     score += 10;
+    document.querySelector("#scr").textContent=score;
+    
+}
+document.querySelector("#pbot").addEventListener("click",function(dets){
+    var clicked = Number(dets.target.textContent);   
+    if(clicked===hitrn){
+        increaseScore();
+        Hit();
+        makeBubble();
+    } 
+})
+Hit();
+runTimer();
+makeBubble();
+
